971a7fd2e78cbd5b0f13cd062419b539f6361ec3
Caio-Moreno <cvinicius.moreno@outlook.com>
2021-02-27 19:20:52 -0300

    Primeiro JSON

---
+++ src/main/java/br/com/brazukas/Models/Index.java
@@ -0,0 +1,11 @@
+package br.com.brazukas.Models;
+
+public class Index {
+    public String Mensagem;
+    public String Nome;
+
+    public Index(String Mensagem, String Nome){
+        this.Mensagem = Mensagem;
+        this.Nome = Nome;
+    }
+}
--- src/main/java/br/com/brazukas/controller/IndexController.java
+++ src/main/java/br/com/brazukas/controller/IndexController.java
@@ -1,16 +1,19 @@
 package br.com.brazukas.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import br.com.brazukas.Models.*;
 
 @Controller
 public class IndexController {
-	
-	@RequestMapping("/")
+
+	@RequestMapping(method = RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
-	public String hello() {
-		return "Hello World";
+	public Index hello() {
+		return new Index("Mensagem", "Teste");
 	}
 	
 }

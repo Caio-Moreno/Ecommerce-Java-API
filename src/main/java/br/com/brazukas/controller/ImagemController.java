8409741b9d9ec7b8fdd66bbc11319abf0a803f9e
joaovitor2019 <vitor.alves99@yahoo.com>
2021-03-06 19:38:24 -0300

    clear code

--- src/main/java/br/com/brazukas/controller/ImagemController.java
+++ src/main/java/br/com/brazukas/controller/ImagemController.java
@@ -1,25 +1,13 @@
 package br.com.brazukas.controller;
 
-import br.com.brazukas.DAO.ProdutoDAO;
 import br.com.brazukas.Models.Imagem;
-import br.com.brazukas.Models.Produto;
-import br.com.brazukas.controller.Dto.ProdutoDto;
 import br.com.brazukas.controller.infra.FileServer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.awt.*;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.Arrays;
 import java.util.List;
-import java.util.logging.Level;
-
-import static br.com.brazukas.Util.CriarArquivoDeLog.gravaLog;
 
 @CrossOrigin
 @RestController

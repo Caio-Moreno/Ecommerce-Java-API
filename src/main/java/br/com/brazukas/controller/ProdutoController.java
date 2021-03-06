79370ace9b0ff4e0607655844fa49fe108133895
Caio-Moreno <cvinicius.moreno@outlook.com>
2021-03-06 11:10:14 -0300

    Update ProdutoController.java

--- src/main/java/br/com/brazukas/controller/ProdutoController.java
+++ src/main/java/br/com/brazukas/controller/ProdutoController.java
@@ -1,27 +1,17 @@
 package br.com.brazukas.controller;
 
-import br.com.brazukas.DAO.ClienteDAO;
 import br.com.brazukas.DAO.ProdutoDAO;
-import br.com.brazukas.Models.Cliente;
+import br.com.brazukas.Models.Responses.ProdutoResponse;
+import br.com.brazukas.Models.Responses.ProdutoResponseDto;
 import br.com.brazukas.controller.Dto.ProdutoDto;
 import br.com.brazukas.Models.Produto;
-import br.com.brazukas.controller.infra.FileServer;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.server.ResponseStatusException;
-
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-
-import static br.com.brazukas.Util.CriarArquivoDeLog.gravaLog;
 
 
 @CrossOrigin
@@ -29,90 +19,69 @@ import static br.com.brazukas.Util.CriarArquivoDeLog.gravaLog;
 @RequestMapping("/Produtos")
 public class ProdutoController {
 
-    private String nome;
-
-    @Autowired
-    private FileServer fileSaver;
-
     @ApiOperation(value = "Retorna uma lista de produtos")
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Retorna a lista de produtos"),
-            @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
-            @ApiResponse(code = 500, message = "Foi gerada uma exceção"),
-    })
     @RequestMapping(method = RequestMethod.GET)
-    public List<ProdutoDto> ListaTodosProdutos() throws IOException, SQLException {
-
-        gravaLog("nome->>"+nome, "padrao", Level.INFO);
-        List<ProdutoDto> lista = ProdutoDAO.consultarProduto();
-
-        return  lista;
+    public ProdutoResponseDto ListaTodosProdutos() throws IOException, SQLException {
+        List<ProdutoDto> listaProd =ProdutoDAO.consultarProduto();
+        return new ProdutoResponseDto(200,listaProd.size()+" Produtos encontrados", listaProd);
     }
 
     @ApiOperation(value = "Retorna uma lista de produtos filtros por nome")
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Retorna a lista de produtos filtrados por nome"),
-            @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
-            @ApiResponse(code = 500, message = "Foi gerada uma exceção"),
-    })
     @RequestMapping(params = {"Nome"}, method = RequestMethod.GET)
-    public List<Produto> ListaProdutosPorNome(String Nome) throws IOException, SQLException {
-        List<Produto> lista = ProdutoDAO.consultaProdutoPorNome(Nome);
-        return  lista;
+    public ProdutoResponse ListaProdutosPorNome(String Nome) throws IOException {
+        List<Produto> listaProdutos =  ProdutoDAO.consultaProdutoPorNome(Nome);
+
+
+        return new ProdutoResponse(200, listaProdutos.size()+" Produtos encontrados", listaProdutos);
     }
 
     @ApiOperation(value = "Insere um produto")
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Retorna o produto inserido"),
-            @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
-            @ApiResponse(code = 500, message = "Foi gerada uma exceção"),
-    })
     @RequestMapping(method =  RequestMethod.POST)
-    public ProdutoDto InserirProduto(@RequestBody Produto produto, List<MultipartFile> fotos) throws IOException, SQLException {
-        boolean inseriu = ProdutoDAO.inserirProduto(produto);
-        if(inseriu) {
-            return ProdutoDAO.retornarUltimoProduto();
-        }else{
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Erro para inserir produto");
+    public ProdutoResponseDto InserirProduto(@RequestBody Produto produto) throws IOException, SQLException {
+                boolean inseriu = ProdutoDAO.inserirProduto(produto);
+                List<ProdutoDto> list = new ArrayList<>();
+
+                if(inseriu) {
+                    ProdutoDto prod =  ProdutoDAO.retornarUltimoProduto();
+                    list.add(prod);
+                    return  new ProdutoResponseDto(200,"Produto inserido com sucesso",list);
+                }else{
+
+                    return new ProdutoResponseDto(500,"Erro para inserir o produto",null);
+                }
+            }
+
+            @ApiOperation(value = "Atualiza o produto recebendo o parametro Id")
+            @RequestMapping(params = {"Id"}, method = RequestMethod.PUT)
+            public ProdutoResponse atualizaCliente(@RequestBody Produto produto, int Id ) throws IOException {
+                boolean inseriu = ProdutoDAO.atualizarProduto(produto, Id);
+                List<Produto> list = new ArrayList<>();
+                if(inseriu){
+                    Produto prod = ProdutoDAO.consultaProdutoPorId(Id);
+                    if (prod == null) {
+                        return new ProdutoResponse(404, "Produto não encontrado na base", null);
+                    }
+                    list.add(prod);
+                    return new ProdutoResponse(200, "Produto atualizado com sucesso", list);
+                }else{
+                    return new ProdutoResponse(500, "Erro para atualizar o produto", null);
+                }
+            }
+            @ApiOperation(value = "Deleta o produto recebendo o parametro Id")
+            @RequestMapping(params = {"Id"}, method = RequestMethod.DELETE)
+            public ProdutoResponse DeletaCliente(int Id) throws IOException {
+                Produto prod = ProdutoDAO.consultaProdutoPorId(Id);
+
+                if (prod == null) {
+                    return new ProdutoResponse(404, "Produto não encontrado na base", null);
+                }
+
+                boolean deletou = ProdutoDAO.deletarCliente(Id);
+
+                if(deletou){
+                    return new ProdutoResponse(200, "Produto deletado com sucesso", null);
+                }else{
+                    return new ProdutoResponse(500, "Erro para deletar produto", null);
+                }
+            }
         }
-    }
-
-    @ApiOperation(value = "Atualiza o produto recebendo o parametro Id")
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Retorna o produto atualizado"),
-            @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
-            @ApiResponse(code = 500, message = "Foi gerada uma exceção"),
-    })
-    @RequestMapping(params = {"Id"}, method = RequestMethod.PUT)
-    public Produto atualizaCliente(@RequestBody Produto produto, int Id ) throws IOException, SQLException {
-
-        boolean inseriu = ProdutoDAO.atualizarProduto(produto, Id);
-        if(inseriu){
-            return ProdutoDAO.consultaProdutoPorId(Id);
-        }else{
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Erro para atualizar produto");
-        }
-    }
-    @ApiOperation(value = "Deleta o produto recebendo o parametro Id")
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Retorna a confirmação do produto"),
-            @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
-            @ApiResponse(code = 500, message = "Foi gerada uma exceção"),
-    })
-    @RequestMapping(params = {"Id"}, method = RequestMethod.DELETE)
-    public String DeletaCliente(int Id) throws IOException, SQLException {
-        Produto prod = ProdutoDAO.consultaProdutoPorId(Id);
-
-        if (prod == null) {
-            return "Erro para deletar produto, o mesmo não existe mais";
-        }
-
-        boolean deletou = ProdutoDAO.deletarCliente(Id);
-
-        if(deletou){
-            return "Produto deletado com sucesso!";
-        }else{
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Erro para deletar produto");
-        }
-    }
-}
